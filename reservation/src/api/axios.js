be1f3be6e27afc9d3e7e0c0663b9caf68c67cefc
halo0516 <ywang20@seas.upenn.edu>
2023-01-16 17:40:29 -0500

    Web name changed with icon

--- reservation/src/api/axios.js
+++ reservation/src/api/axios.js
@@ -11,7 +11,7 @@ export default axios.create({
 export const register = async (newUser) => {
     try {
       // const response = await axios.post(`${baseURL}/users`, newUser);
-      const response = await axios.post('http://localhost:3500/account',newUser);
+      const response = await axios.post('http://localhost:3500/account', newUser);
       return response;
     } catch (err) {
       throw new Error(err);
